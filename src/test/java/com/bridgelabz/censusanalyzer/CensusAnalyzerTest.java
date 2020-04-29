9176e94f31305f50bd7da5ed515857f6cfc1e08e
PalakPartani <partanipalak16@gmail.com>
2020-04-29 03:17:47 -0700

    ADDED:feature to throw exception when wrong file type passed

--- src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
@@ -6,8 +6,8 @@ import org.junit.rules.ExpectedException;
 
 public class CensusAnalyzerTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-    private static final String WRONG_CSV_FILE_PATH="./src/main/resources/IndiaStateCensusData.csv";
-
+    private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataa.csv";
+    private static final String WRONG_CSV_FILE_TYPE = "./src/test/resouresc/IndiaStateCensusData.json";
     @Test
     public void givenStateCensusCsvFile_checkToEnsure_returnNumberOfRecords() throws CensusAnalyserException {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
@@ -23,5 +23,17 @@ public class CensusAnalyzerTest {
         } catch(CensusAnalyserException e){
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_PROBLEM,e.type);
         }
+    }
+    @Test
+    public void givenStateCensusCsvFile_WhenTypeIncorrect_shouldReturnException() {
+        try {
+            CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+            ExpectedException expectedException = ExpectedException.none();
+            expectedException.expect(ClassCastException.class);
+            censusAnalyzer.loadCensusData(WRONG_CSV_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_PROBLEM,e.type);
+        }
+
     }
 }
